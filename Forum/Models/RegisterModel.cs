88cf879adb574f7c22ad23f3afdc788e2471469d
Максим Борисенко <bmaksm@live.ru>
2019-04-08 18:25:50 +0300

    Corrected RegisterModel

--- Forum/Models/RegisterModel.cs
+++ Forum/Models/RegisterModel.cs
@@ -18,14 +18,18 @@ namespace Forum.Models {
         public string Email { get => email; set => email = value; }
 
         [Required]
+        [MaxLength(20, ErrorMessage = "Максимум 20 символов")]
+        [MinLength(5, ErrorMessage = "Минимум 5 символов")]
         public string Username { get => username; set => username = value; }
 
         [Required]
+        [MinLength(6, ErrorMessage = "Минимум 5 символов")]
         [DataType(DataType.Password)]
         public string Password { get => password; set => password = value; }
 
         [Required]
-        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [MinLength(6, ErrorMessage = "Минимум 5 символов")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
         [DataType(DataType.Password)]
         public string PasswordConfirm { get => passwordConfirm; set => passwordConfirm = value; }
     }
